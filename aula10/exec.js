37bd31e1e3305c279053bb4ad09294b14f5c5293
amaral <119061935+amxral@users.noreply.github.com>
2024-03-27 18:27:18 -0300

    Create exec.js

---
+++ aula10/exec.js
@@ -0,0 +1,12 @@
+
+var n = 1
+while (n <= 10) {
+    console.log(`while - Vamos contar: ${n}`)
+    n++
+} 
+
+//Mesmo resultado com modelos diferentes 
+
+for (var n=1; n<=10; n++) {
+    console.log(`for - Vamos contar: ${n}`)
+}

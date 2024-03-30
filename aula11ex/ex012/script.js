c567284e7ae54ae1eda60d1190acfc4d0d96dcb1
amaral <119061935+amxral@users.noreply.github.com>
2024-03-30 20:20:41 -0300

    criei exercicio de tabuada

--- aula11ex/ex012/script.js
+++ aula11ex/ex012/script.js
@@ -1,1 +1,15 @@
-alert('Olá Mundo')
+function clicou() {
+    let num = document.getElementById('nmr')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text
+        }
+    }
+}

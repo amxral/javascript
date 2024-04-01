f42d6283a6577b779c282b5057dce8d7814551fe
amaral <119061935+amxral@users.noreply.github.com>
2024-04-01 20:51:48 -0300

    exerc tabuada

--- aula11ex/ex012/script.js
+++ aula11ex/ex012/script.js
@@ -7,9 +7,18 @@ function clicou() {
     } else {
         let n = Number(num.value)
         let c = 1
-        while (c <= 10) {
+        tab.innerHTML = ''
+        for (let c = 1 ; c <= 10; c++) {
             let item = document.createElement('option')
-            item.text
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
         }
+        /*while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }*/
     }
 }
